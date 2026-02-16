6669aed8dcb3ed2304fe1041ebbc01d8f0834f86
Raidenuh <camposanochanaiden@gmail.com>
2026-02-16 17:13:30 -0500

    Update ModelSwitching

--- Assets/Scenes/ModelSwitching.cs
+++ Assets/Scenes/ModelSwitching.cs
@@ -8,17 +8,29 @@ public class ModelSwitching : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        SwapModels(false);
     }
 
-    public void On()
+    public void SwapModels(bool strangified)
     {
-        
+        if (strangified)
+        {
+            regular.SetActive(false);
+            strange.SetActive(true);
+        } else
+        {
+            regular.SetActive(true);
+            regular.SetActive(false);
+        }
+        Debug.Log(strangified);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.frameCount < 300)
+        {
+            SwapModels(true);
+        }
     }
 }
