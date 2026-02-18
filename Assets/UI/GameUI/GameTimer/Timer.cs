42aab2c9d81bc63d322739ea7a92307043a13d87
Nic_In_Time <nicduclos9@gmail.com>
2026-02-18 10:22:29 -0500

    added formatting, made end of timer actually load victory screen, added victory screen scene

--- Assets/UI/GameUI/GameTimer/Timer.cs
+++ Assets/UI/GameUI/GameTimer/Timer.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
 using TMPro;
+using System;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
     private float timeLeft;
+
     public static Timer Instance { get; private set; }
 
     [SerializeField] private TMP_Text text;
@@ -21,7 +24,35 @@ public class Timer : MonoBehaviour
     void Update()
     {
         timeLeft -= Time.deltaTime;
-        text.text = timeLeft.ToString();
+        if (timeLeft <= 0)
+        {
+            OpenVictoryScreen();
+        }
+        TimeSpan time = TimeSpan.FromSeconds(timeLeft);
+        if (timeLeft >= 0) 
+        {
+            if (time.Seconds > 9f)
+            {
+                if (time.Milliseconds > 99f)
+                {
+                    text.text = time.Minutes.ToString() + ":" + time.Seconds.ToString() + "." + time.Milliseconds.ToString();
+                } 
+                else if (time.Milliseconds > 9f)
+                {
+                    text.text = time.Minutes.ToString() + ":" + time.Seconds.ToString() + ".0" + time.Milliseconds.ToString();
+                } 
+                else
+                {
+                    text.text = time.Minutes.ToString() + ":" + time.Seconds.ToString() + ".00" + time.Milliseconds.ToString();
+                }
+            } 
+            else
+            {
+                text.text = time.Minutes.ToString() + ":0" + time.Seconds.ToString() + "." + time.Milliseconds.ToString();
+            }
+        }
     }
+
+    public void OpenVictoryScreen() => SceneManager.LoadScene("VictoryMenuScene");
 
 }
