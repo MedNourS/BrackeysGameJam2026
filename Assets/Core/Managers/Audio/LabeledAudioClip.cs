7cb162619b55ca034796fafbb07dcff699b24683
Nour Sabir <sabir.med.nour@gmail.com>
2026-02-21 22:48:23 -0500

    Changed Managers Prefab to include list of labeled audio clips

--- Assets/Core/Managers/Audio/LabeledAudioClip.cs
+++ Assets/Core/Managers/Audio/LabeledAudioClip.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 
 // Labeled AudioClip so it can be accessed with PlaySFX type commands
-public class LabeledAudioClip 
+[System.Serializable]
+public class LabeledAudioClip
 {
     public string name;
     public AudioClip audioClip;
